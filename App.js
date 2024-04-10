7dcb0f4264173a07561a0bb9b12a7463c7516a2f
Jhony Freitas <jhony00._@hotmail.com>
2024-04-10 07:40:37 -0300

    Criando projeto de navegação

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Contacts from "./src/pages/Contacts";
+import Information from "./src/pages/Information";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App (){
+  const Stack = createStackNavigator(); 
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Contacts" component={Contacts}/>
+        <Stack.Screen name="Information" component={Information}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ src/pages/Information/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Information (){
+  return(
+    <View>
+      <Text>Contacts</Text>
+    </View>
+  );
+};
